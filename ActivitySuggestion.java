a3a81bef5cf67f795ab940b401c0c8d744a1cf0b
Anthony Carpenetti <31355951+skiller185@users.noreply.github.com>
2017-10-24 13:46:04 -0400

    Adjusting to Fix Bugs (#3)
    * Update ActivitySuggestion.java
    
    * Added Class File

--- ActivitySuggestion.java
+++ ActivitySuggestion.java
@@ -1,11 +1,12 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class ActivitySuggestion
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
 		//input Scanner calls from console
 		//fileRead Scanner calls from the File set by the first argument
@@ -29,25 +30,32 @@ public class ActivitySuggestion
 			remainingActivities = activities.size();
 			int choice = rand.nextInt(remainingActivities);
 			boolean responseInvalid = true;
-			char response;
+			char response = 'a';
 			while(responseInvalid)
 			{
 				System.out.println("Would you like to " + activities.get(choice) + " (y/n)?");
 				String decision = input.nextLine();
-				response = decision.charAt(0);
-				if(response == 'y')
+				if(decision == "")
 				{
-					responseInvalid = false;
-					rejected = false;
-				}
-				else if(response == 'n')
-				{
-					responseInvalid = false;
+					System.out.println("Hello? You have to say something... I'm not a mind reader.\n");
 				}
 				else
 				{
-					System.out.println("I'm sorry. I don't understand what you meant by that. Could you say that differently?");
-					System.out.println("(Respond with the letter y or n.)");
+					response = decision.charAt(0);
+					if(response == 'y')
+					{
+						responseInvalid = false;
+						rejected = false;
+					}
+					else if(response == 'n')
+					{
+						responseInvalid = false;
+					}
+					else
+					{
+						System.out.println("I'm sorry. I don't understand what you meant by that. Could you say that differently?");
+						System.out.println("(Respond with the letter y or n.)");
+					}
 				}
 			}
 			if(response == 'n')
