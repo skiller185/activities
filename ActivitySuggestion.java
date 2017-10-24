8fe23d241dbe9b7dee8b514f740097e7629fb19c
Anthony Carpenetti <31355951+skiller185@users.noreply.github.com>
2017-10-24 09:11:04 -0400

    Update ActivitySuggestion.java

--- ActivitySuggestion.java
+++ ActivitySuggestion.java
@@ -7,14 +7,24 @@ public class ActivitySuggestion
 {
 	public static void main(String[] args)
 	{
+		//input Scanner calls from console
+		//fileRead Scanner calls from the File set by the first argument
 		Scanner input = new Scanner(System.in);
 		Scanner fileRead = new Scanner(new File(args[0]));
 		
 		ArrayList<String> activities = new ArrayList<String>();
+		int activitiesSize = activities.size();
 		
-		while(fileRead.hasNext())
+		for(int i = 0;fileRead.hasNext();i++)
 		{
-			
+			if(activiesSize - 1 <= i)
+			{
+				
+			}
+			else
+			{
+				activities.set(i, );
+			}
 		}
 	}
 }
