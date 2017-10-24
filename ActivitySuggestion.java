3d5ce3667ed62cf8ebdf2446775d6dc8ad374992
Anthony Carpenetti <31355951+skiller185@users.noreply.github.com>
2017-10-24 09:01:19 -0400

    Create ActivitySuggestion.java

---
+++ ActivitySuggestion.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ActivitySuggestion
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		Scanner fileRead = new Scanner(new File(args[0]));
+		
+		ArrayList<String> activities = new ArrayList<String>();
+		
+		while(fileRead.hasNext())
+		{
+			
+		}
+	}
+}
